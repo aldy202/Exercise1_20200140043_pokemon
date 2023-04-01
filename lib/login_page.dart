3eabee0754e236b54c00792d60982817419ddd08
aldy202 <aldyfauzi202@.gmail.com>
2023-04-02 01:33:16 +0700

    pembuatan input login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -18,7 +18,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 55),
           child: Form(
@@ -50,6 +50,109 @@ class _LoginPageState extends State<LoginPage> {
                         fontSize: 15,
                         color: Colors.grey,
                         fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.name,
+                    controller: namaController,
+                    decoration: InputDecoration(
+                        labelText: "Name",
+                        hintText: "Enter your name",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.person)),
+                    validator: (value) {
+                      bool nameValid = RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value!);
+
+                      if (value.isEmpty) {
+                        return "Please enter your name";
+                      }
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: passToggel,
+                    controller: passController,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Enter your password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffix: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggel = !passToggel;
+                          });
+                        },
+                        child: Icon(passToggel
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter your password";
+                      } else if (passController.text.length < 6) {
+                        return "Please must be atleast 6 characters";
+                      }
+                    },
+                  ),
+                  SizedBox(height: 40),
+                  InkWell(
+                    onTap: () {
+                      if (_formfield.currentState!.validate()) {
+                        print("Success");
+                        namaController.clear();
+                        passController.clear();
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Center(
+                          child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account ? ",
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterPage()),
+                            );
+                          },
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ))
+                    ],
                   )
                 ]),
           )),
