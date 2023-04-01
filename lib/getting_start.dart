ad9d37be61ac412bf24d9f2200b4af7adfe84f2d
aldy202 <aldyfauzi202@.gmail.com>
2023-04-01 23:40:07 +0700

    pembuatan text di getting starting

--- lib/getting_start.dart
+++ lib/getting_start.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -6,6 +7,33 @@ class GettingStart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            "Pokedex.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 40, fontWeight: FontWeight.bold, color: Colors.amber),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Wellcome to Pokedex",
+            style: TextStyle(fontSize: 15, color: Colors.black),
+          ),
+          Text(
+            "You can find your favorite pokemon here",
+            style: TextStyle(fontSize: 15, color: Colors.black),
+          ),
+        ],
+      ),
+    ));
   }
 }
