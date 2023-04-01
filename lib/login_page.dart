4b7ebdbbc263bc72290dcb83ccac0f8c2081924d
aldy202 <aldyfauzi202@.gmail.com>
2023-04-02 00:59:21 +0700

    menambahkan text

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,23 +11,50 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formfield = GlobalKey<FormState>();
-  final emailController = TextEditingController();
+  final namaController = TextEditingController();
   final passController = TextEditingController();
   bool passToggel = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Form(
-            child: Form(
-          key: _formfield,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center, children: []),
-        )),
-      )),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 55),
+          child: Form(
+              child: Form(
+            key: _formfield,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("asset/image/pokemon_logo.png",
+                      height: 200, width: 350),
+                  Text(
+                    "Let's get started.",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "There's No Sense In Going Out Of",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    "You Way To Get Somebody To Like You",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w700),
+                  )
+                ]),
+          )),
+        ),
+      ),
     );
   }
 }
