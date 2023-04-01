a480770e9b66f1893592ac8de80174d9ac41c9a6
aldy202 <aldyfauzi202@.gmail.com>
2023-04-02 05:12:31 +0700

    penambahan hide and show password

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'login_page.dart';
+
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -14,6 +16,7 @@ class _RegisterPageState extends State<RegisterPage> {
   final _usernameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool passToggle = true;
 
   @override
   void dispose() {
@@ -27,14 +30,188 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
-        child: Form(
-          key: _formkey,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center, children: [CircleAvatar()]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 45),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(0),
+                  child: Row(children: [
+                    FloatingActionButton(
+                      child: Icon(Icons.arrow_back),
+                      backgroundColor: Colors.amber,
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
+                      },
+                    )
+                  ]),
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage("asset/image/pikachu.jpg"),
+                  radius: 60,
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Text(
+                  "Register",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.amber,
+                  ),
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 4),
+                  child: TextFormField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: "Name",
+                      prefixIcon: Icon(Icons.person),
+                      hintText: "Enter your name",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your name";
+                      } else if (value.length < 6) {
+                        return "Name must be atleast 6 characters";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 4),
+                  child: TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Icon(Icons.email),
+                      hintText: "Enter your email",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your email";
+                      } else if (!value.contains("@") || !value.contains(".")) {
+                        return "Please enter a valid email address";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 4),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: passToggle,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Enter your password",
+                      prefixIcon: Icon(Icons.lock),
+                      suffix: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: Icon(passToggle
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter your password";
+                      } else if (value.length < 6) {
+                        return "Password must be atleast 6 characters";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 4),
+                  child: TextFormField(
+                    obscureText: passToggle,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: "Re-Password",
+                      hintText: "Re-Enter your password",
+                      suffix: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: Icon(passToggle
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please re-enter your password";
+                      } else if (value.length < 6) {
+                        return "Password must be atleast 6 characters";
+                      } else if (value != _passwordController.text) {
+                        return "Password must be the same";
+                      }
+                      return null;
+                    },
+                  ),
+                  ),
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(vertical: 32.0),
+                //   child: ElevatedButton(
+                //     style: ElevatedButton.styleFrom(
+                //       padding: const EdgeInsets.symmetric(vertical: 10),
+                //     ),
+                //     onPressed: () {
+                //       if (_formKey.currentState!.validate()) {
+                //         Navigator.push(
+                //           context,
+                //           MaterialPageRoute(
+                //             builder: (context) => Dashboard(User(
+                //                 _usernameController.text,
+                //                 _emailController.text,
+                //                 _passwordController.text)),
+                //           ),
+                //         );
+                //       }
+                //     },
+                //     child: SizedBox(
+                //       width: double.infinity,
+                //       height: 40,
+                //       child: ElevatedButton(
+                //         child: Text("Register"),
+                //         onPressed: () {},
+                //       ),
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
