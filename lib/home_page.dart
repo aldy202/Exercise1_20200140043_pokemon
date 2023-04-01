b2c4e0a7f7a1e9d89fd152e0adbaabbc67f4dd0a
aldy202 <aldyfauzi202@.gmail.com>
2023-04-02 05:16:08 +0700

    tambah file home page

---
+++ lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
