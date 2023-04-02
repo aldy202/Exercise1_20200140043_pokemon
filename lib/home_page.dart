a820cd7af5eea7954fa8d565f2a35a21333635c1
aldy202 <aldyfauzi202@.gmail.com>
2023-04-02 08:15:36 +0700

    commit home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,57 +10,51 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false, // Tidak menampilkan tombol kembali
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {},
-          ),
-        ],
-      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            child: Padding(
-              padding: EdgeInsets.only(left: 20.0),
-              child: Row(children: [
-                RichText(
-                    text: TextSpan(
-                        text: "Hallo, ",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                        children: <TextSpan>[
-                      TextSpan(
-                          text: '$nama',
+            child: Container(
+              child: Padding(   
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0), 
+                child: Row(children: [
+                  RichText(
+                      text: TextSpan(
+                          text: "Hallo, ",
                           style: TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.bold,
-                          ))
-                    ])),
-                SizedBox(
-                  width: 500.0,
-                ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.person))
-              ]),
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                          children: <TextSpan>[
+                        TextSpan(
+                            text: '$nama',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ))
+                      ])),
+                  SizedBox(
+                    width: 210.0,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("asset/image/profil.png"),
+                    radius:17,
+                  )
+                ]),
+              ),
             ),
           ),
-          SizedBox(height: 20.0),
-          Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text(
-              "Pokedex",
-              style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.yellow),
+          SizedBox(height: 8.0),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "Pokedex",
+                style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.yellow),
+              ),
             ),
           ),
           SizedBox(
@@ -85,18 +79,23 @@ class MyWidget extends StatelessWidget {
           SizedBox(height: 20.0),
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: 4,
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    leading: Icon(Icons.person),
-                    title: Text("Item $index"),
-                    subtitle: Text("Deskripsi item $index"),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                    onTap: () {},
+                return Container(
+                  width: 50.0,
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text("Item $index"),
+                      subtitle: Text("Deskripsi item $index"),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () {},
+                      contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 12.0)
+                    ),
                   ),
                 );
               },
+              
             ),
           ),
         ],
